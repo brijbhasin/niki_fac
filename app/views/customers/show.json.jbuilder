b1a881413a10ddabf16459edcb14af0df04fe076
Prelang Builder <builder@prelang.com>
2016-02-05 05:37:30 +0000

    Generated scaffolding for model 'Customer' via Rails

---
+++ app/views/customers/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @customer, :id, :name, :contact_person, :contact_phone, :location, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :customers
+
   resources :projects
 
   resources :project_activities
